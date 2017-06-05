6a2ca3fe6b0207698a99e4d7fc23d4b64d8fb8d0
Сергей Житенев < >
2017-06-05 15:33:54 +0300

    portfolio

---
+++ js/sidenav.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+
+    var sidenavIsOpen = false;
+
+    function openNav() {
+
+        document.querySelector(".sidenav").style.width = "250px";
+        document.querySelector(".content").style.marginLeft = "250px";
+        document.querySelector('body').style.overflow = 'hidden';
+        document.querySelector('body').style.height = '100%';
+
+        return false;
+    }
+
+    function closeNav() {
+
+        document.querySelector(".sidenav").style.width = "0";
+        document.querySelector(".content").style.marginLeft = "0";
+        document.querySelector('body').style.overflow = 'initial';
+        document.querySelector('body').style.height = 'initial';
+
+        return false;
+
+    }
+
+    function handler() {
+
+        sidenavIsOpen = !sidenavIsOpen;
+
+        sidenavIsOpen ? openNav() : closeNav();
+
+    }
+
+    document.querySelector('.mobilemenu-button').addEventListener('click', handler);
+    document.querySelector('.mobilemenu-button').addEventListener('touchstart', handler);
+
+};
